08d026d7f1ac931cececf7dea5ef0d97b22aa988
daisho <daishokomiyama@gmail.com>
2019-11-14 23:20:54 -0500

    Renaming variables, fixing touch device specific bug: once a tag moved, it can move again with touch

--- sites/drag-drop/js/main.js
+++ sites/drag-drop/js/main.js
@@ -1,13 +1,13 @@
 const dd = {};
 
-dd.setOrder = function (element) {
+dd.setOrder = element => {
     element && element.removeAttribute("style");
-    dd.lastPos && dd.empties[dd.lastPos].classList.remove("hovered");
-    dd.fills = document.querySelectorAll(".fill");
+    dd.lastPosition && dd.spots[dd.lastPosition].classList.remove("hovered");
+    dd.tags = document.querySelectorAll(".fill");
 };
 
 dd.dragStart = function () {
-    dd.grabbed = this;
+    dd.currentTag = this;
     setTimeout(() => this.classList.add("invisible"), 0);
     dd.indexFrom = parseInt(this.parentElement.getAttribute("data-position"));
 };
@@ -24,77 +24,67 @@ dd.dragOver = function (event) {
 dd.dragEnter = function () {
     event.preventDefault();
     dd.indexTo = parseInt(this.getAttribute("data-position"));
-    setTimeout(() => dd.adjustTop(dd.indexFrom < dd.indexTo ? "up" : "down"), 0);
+    setTimeout(() => dd.pushTags(dd.indexFrom < dd.indexTo), 0);
 };
 
 dd.dragLeave = function () {
     this.classList.remove("hovered");
-    dd.adjustTop();
+    for (let i = 0; i < dd.tags.length; i++) {
+        dd.tags[i].classList.remove("up", "down");
+    }
 };
 
 dd.dragDrop = function () {
     this.classList.remove("hovered");
     dd.removeUpDownFromFills();
-    dd.appendAll(parseInt(this.getAttribute("data-position")));
-    this.append(dd.grabbed);
+    dd.dropTags(parseInt(this.getAttribute("data-position")));
+    this.append(dd.currentTag);
     setTimeout(() => dd.setOrder(), 0);
 };
 
-dd.appendAll = function (indexTo) {
-    if (dd.indexFrom < indexTo) {
-        for (let i = dd.indexFrom + 1; i <= indexTo; i++) {
-            dd.empties[i - 1].append(dd.fills[i]);
-        }
-    }
-    else {
-        for (let i = dd.indexFrom - 1; i >= indexTo; i--) {
-            dd.empties[i + 1].append(dd.fills[i]);
+dd.dropTags = indexTo => {
+    dd.indexFrom < indexTo ? appendTags(dd.indexFrom + 1, indexTo, -1) : appendTags(indexTo, dd.indexFrom - 1, 1);
+
+    function appendTags (initialIndex, endIndex, offset) {
+        for (let i = initialIndex; i <= endIndex; i++) {
+            dd.spots[i + offset].append(dd.tags[i]);
         }
     }
 };
 
-dd.adjustTop = function (operator) {
-    if (operator === "up") {
-        for (let i = dd.indexFrom + 1; i <= dd.indexTo; i++) {
-            dd.fills[i].classList.add("up");
-        }
-    }
-    else if (operator === "down") {
-        for (let i = dd.indexTo; i < dd.indexFrom; i++) {
-            dd.fills[i].classList.add("down");
-        }
-    }
-    else {
-        for (let i = 0; i < dd.fills.length; i++) {
-            dd.fills[i].classList.remove("up", "down");
+dd.pushTags = downwards => {
+    downwards ? addClassName("up", dd.indexFrom + 1, dd.indexTo) : addClassName("down", dd.indexTo, dd.indexFrom - 1);
+
+    function addClassName (className, initialIndex, endIndex) {
+        for (let i = initialIndex; i <= endIndex; i++) {
+            dd.tags[i].classList.add(className);
         }
     }
 }
 
-dd.init = function () {
-    dd.empties = document.getElementsByClassName("empty");
-    dd.fills = "";
-    dd.grabed = "";
+dd.init = () => {
+    dd.spots = document.getElementsByClassName("empty");
+    dd.tags = "";
+    dd.currentTag = "";
     dd.indexFrom = "";
     dd.indexTo = "";
-    dd.hoverOn = "";
     dd.setOrder();
-    for (let i = 0; i < dd.fills.length; i++) {
-        dd.fills[i].addEventListener('dragstart', dd.dragStart);
-        dd.fills[i].addEventListener('dragend', dd.dragEnd);
+    for (let i = 0; i < dd.tags.length; i++) {
+        dd.tags[i].addEventListener('dragstart', dd.dragStart);
+        dd.tags[i].addEventListener('dragend', dd.dragEnd);
     }
-    for (let i = 0; i < dd.empties.length; i++) {
-        dd.empties[i].addEventListener('dragover', dd.dragOver);
-        dd.empties[i].addEventListener('dragenter', dd.dragEnter);
-        dd.empties[i].addEventListener('dragleave', dd.dragLeave);
-        dd.empties[i].addEventListener('drop', dd.dragDrop);
+    for (let i = 0; i < dd.spots.length; i++) {
+        dd.spots[i].addEventListener('dragover', dd.dragOver);
+        dd.spots[i].addEventListener('dragenter', dd.dragEnter);
+        dd.spots[i].addEventListener('dragleave', dd.dragLeave);
+        dd.spots[i].addEventListener('drop', dd.dragDrop);
     }
 };
 
 dd.init();
 
 // ************************ TOUCH ***********
-dd.getPosition = function (x, y) {
+dd.getPosition = (x, y) => {
     for (let i = 0; i < dd.coordinates.length; i++) {
         if ((x >= dd.coordinates[i].x && x <= dd.coordinates[i].right) && (y >= dd.coordinates[i].y && y <= dd.coordinates[i].bottom)) {
             return i;
@@ -102,9 +92,9 @@ dd.getPosition = function (x, y) {
     }
 }
 
-dd.removeUpDownFromFills = function () {
-    for (let i = 0; i < dd.fills.length; i++) {
-        dd.fills[i].classList.remove("up", "down");
+dd.removeUpDownFromFills = () => {
+    for (let i = 0; i < dd.tags.length; i++) {
+        dd.tags[i].classList.remove("up", "down");
     }
 }
 
@@ -117,57 +107,58 @@ dd.touchStart = function (event) {
 };
 
 dd.touchMove = function (event) {
+    dd.hasMoved = true;
     event.cancelable ? event.preventDefault() : "";
     window.requestAnimationFrame(() => {
         this.style.transform = `translate(${event.targetTouches[0].pageX - dd.initialX}px, ${event.targetTouches[0].pageY - dd.initialY}px)`;
         dd.indexTo = dd.getPosition(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
         if (typeof dd.indexFrom === "number" && typeof dd.indexTo === "number") {
-            if (!dd.gotIn) {
-                dd.lastPos = dd.indexTo;
-                dd.empties[dd.indexTo].classList.add("hovered");
-                dd.adjustTop(dd.indexFrom < dd.indexTo ? "up" : "down");
-                dd.gotIn = true;
+            if (!dd.wasInside) {
+                dd.lastSpot = dd.indexTo;
+                dd.spots[dd.indexTo].classList.add("hovered");
+                dd.pushTags(dd.indexFrom < dd.indexTo);
+                dd.wasInside = true;
             }
         }
         else {
-            if (dd.gotIn) {
-                dd.empties[dd.lastPos].classList.remove("hovered");
+            if (dd.wasInside) {
+                dd.spots[dd.lastSpot].classList.remove("hovered");
                 dd.removeUpDownFromFills();
-                dd.gotIn = false;
+                dd.wasInside = false;
             }
         }
     });
 };
 
 dd.touchEnd = function () {
+    if (!dd.hasMoved) return false;
     this.removeAttribute("style");
     dd.removeUpDownFromFills();
     if (typeof dd.indexTo === "number") {
-        dd.empties[dd.indexTo].classList.remove("hovered");
-        dd.appendAll(dd.indexTo);
-        dd.empties[dd.indexTo].append(this);
-        dd.currentSpot = dd.indexTo;
+        dd.spots[dd.indexTo].classList.remove("hovered");
+        dd.dropTags(dd.indexTo);
+        dd.spots[dd.indexTo].append(this);
+        dd.hasMoved = false;
     }
     setTimeout(() => {
         dd.setOrder(this);
     }, 100);
 };
 
-dd.initTouch = function () {
+dd.initTouch = () => {
     dd.coordinates = [];
     dd.lastPosition = 0;
-    dd.initialLocation;
-    dd.currentSpot;
-    dd.gotIn;
-    dd.lastPos;
+    dd.wasInside = false;
+    dd.lastSpot = "";
+    dd.hasMoved = false;
 
-    for (let i = 0; i < dd.empties.length; i++) {
-        dd.coordinates.push(dd.empties[i].getBoundingClientRect());
+    for (let i = 0; i < dd.spots.length; i++) {
+        dd.coordinates.push(dd.spots[i].getBoundingClientRect());
     }
-    for (let i = 0; i < dd.fills.length; i++) {
-        dd.fills[i].addEventListener("touchstart", dd.touchStart);
-        dd.fills[i].addEventListener("touchmove", dd.touchMove, {passive: false});
-        dd.fills[i].addEventListener("touchend", dd.touchEnd);
+    for (let i = 0; i < dd.tags.length; i++) {
+        dd.tags[i].addEventListener("touchstart", dd.touchStart);
+        dd.tags[i].addEventListener("touchmove", dd.touchMove, {passive: false});
+        dd.tags[i].addEventListener("touchend", dd.touchEnd);
     }
 };
 
